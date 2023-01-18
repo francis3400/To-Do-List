288f389a192611257bb83fcca3ae0f08dcf9104b
francis3400 <francisuche712@gmail.com>
2023-01-18 12:29:33 -0800

    update package file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 /* eslint-disable quotes */
-import './style.css';
-import display from './to-do.js';
-import { clearCompleted } from './validate.js';
+import "./style.css";
+import display from "./to-do.js";
+import { clearCompleted } from "./validate.js";
 
-const enterBtn = document.querySelector('.enter-btn');
-const clearCheck = document.querySelector('.clear');
+const enterBtn = document.querySelector(".enter-btn");
+const clearCheck = document.querySelector(".clear");
 
-clearCheck.addEventListener('click', () => {
+clearCheck.addEventListener("click", () => {
   const task = display.getTodo();
   const todo = clearCompleted(task);
-  localStorage.setItem('todo', JSON.stringify(todo));
+  localStorage.setItem("todo", JSON.stringify(todo));
   display.loadTodo(todo);
 });
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const task = display.getTodo();
   display.loadTodo(task);
 });
 
-enterBtn.addEventListener('click', () => {
+enterBtn.addEventListener("click", () => {
   display.addTodo();
 });
+
